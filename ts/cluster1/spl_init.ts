6d6d03e5a86cc796dc7ba3a5762e452c5841619b
rishav76dev <rjrishav037@gmail.com>
2025-04-02 10:13:57 +0530

    init

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import { createMint, getOrCreateAssociatedTokenAccount } from '@solana/spl-token';
+import wallet from "./wba-wallet.json"
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -11,9 +11,19 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
     try {
-        // Start here
-        // const mint = ???
+        const mint = await createMint(
+            connection,
+            keypair,
+            keypair.publicKey,
+            null,
+            6,
+        );
+        console.log(`Your mint address is: ${mint}`);
+      
+
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
-})()
+})();
+
+//Agtqgw2MTTjiGeGgixcafQzfTQyDqjNNtsMhiwjyYE5L
--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -1,7 +1,6 @@
 import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
 import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
-
+import wallet from "./wba-wallet.json"
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
@@ -12,18 +11,20 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 const token_decimals = 1_000_000n;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("Agtqgw2MTTjiGeGgixcafQzfTQyDqjNNtsMhiwjyYE5L");
 
 (async () => {
     try {
         // Create an ATA
+        const ata = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey)
         // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+        console.log(`Your ata is: ${ata.address.toBase58()}`);
 
         // Mint to ATA
         // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
+        const mintTx = await mintTo( connection, keypair,mint, ata.address,keypair.publicKey,8000000)
+        console.log(`Your mint txid: ${mintTx}`);
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
-})()
+})();
