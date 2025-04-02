c7461ebfb9b6875297e902871f04f1c6c29795d2
rishav76dev <rjrishav037@gmail.com>
2025-04-02 20:06:30 +0530

    day2turbin

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../dev-wallet.json"
+import wallet from "./wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { 
     createMetadataAccountV3, 
@@ -10,7 +10,7 @@ import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-fo
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("Agtqgw2MTTjiGeGgixcafQzfTQyDqjNNtsMhiwjyYE5L")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -21,28 +21,39 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+         let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint,
+            mintAuthority: signer,
+         }
+       
+        let data: DataV2Args = {
+            name: "My first NFT",
+            symbol: "NFT",
+            uri: "https://arweave.net/123456",
+            sellerFeeBasisPoints: 50,
+            creators: null,
+            collection: null,
+            uses: null,
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        
+         let args: CreateMetadataAccountV3InstructionArgs = {
+             data,
+             isMutable: true,
+             collectionDetails: null,
+         }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        );
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,5 +1,5 @@
 import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import wallet from "./wba-wallet.json"
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,18 +10,30 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("Agtqgw2MTTjiGeGgixcafQzfTQyDqjNNtsMhiwjyYE5L");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("FhKSxP8W8X9pKz4QcLADvjtbJgzLxm64jSMr26Kw3vEg");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        const fromAta = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        const toAta = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to);
+        console.log("fromAta", fromAta.address.toBase58());
 
         // Transfer the new token to the "toTokenAccount" we just created
+        const tx = await transfer(
+            connection,
+            keypair,
+            fromAta.address,
+            toAta.address,
+            keypair,
+            100,
+        )
+        console.log("txId",tx)
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
